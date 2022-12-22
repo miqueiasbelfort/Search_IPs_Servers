3f38b6c6441b3742fb7746ea412b98fcb8059d92
miqueiasbelfort <kawasousa1554@gmail.com>
2022-12-22 19:46:25 -0300

    first

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Um projeto de lina de comando com GO
+* Projeto foi feito no curso de de GoLang na Udemy
+* Com o projeto você pode ver os IPs dos sites da internet e os Servidores em que estão hospedados.
+
+# Como usar?
+
+* Para ver os IPs do site
+ -> comandline ip --host miqueiasbelort.netlify.app
+* Para ver os Servidores
+ -> comandline servidores --host miqueiasbelfort.netlify.app
---
+++ app/app.go
@@ -0,0 +1,67 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser execultada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPs e Nomes de Servidores na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "miqueiasbelfort.netlify.app",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internt",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidores, erro := net.LookupNS(host) // name server
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
+}
